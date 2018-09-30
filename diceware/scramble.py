53de85a67b8218f0ff4d73d67a963ce085b26280
Sören Köhler <soerenkoehler@users.noreply.github.com>
2018-09-30 02:09:25 +0200

    Diceware: Filepermissions Linux

--- diceware/scramble.py
+++ diceware/scramble.py
@@ -3,10 +3,18 @@
 from argparse import ArgumentParser
 from random import seed, randrange
 
+
 def scramble(x):
     if args.isDecode:
         return (ord(x) - ord('a')) % 6 + ord('1')
     return ord(x) - ord('1') + ord('a') + randrange(0, 24, 6)
+
+
+def group(x):
+    if args.isDecode and len(x) > 4:
+        return x[0:4] + " " + group(x[4:])
+    return x
+
 
 parser = ArgumentParser("Diceware String Encode")
 parser.add_argument('-d', action='store_const', dest='isDecode', const=True)
@@ -15,4 +23,4 @@ args = parser.parse_args()
 
 seed()
 
-print("".join([chr(scramble(x)) for x in args.input]))
+print(group("".join([chr(scramble(x)) for x in args.input.replace(' ', '')])))
