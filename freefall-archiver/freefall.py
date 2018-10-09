3126da006b44976d43a61c0ef0e1a15ea44e89a3
Sören Köhler <soerenkoehler@users.noreply.github.com>
2018-10-09 19:37:51 +0200

    Freefall: Bild-URL speichern

--- freefall-archiver/freefall.py
+++ freefall-archiver/freefall.py
@@ -15,18 +15,28 @@ def find(regexp, text):
 
 def main():
     freefall = []
+
+    print("--- scanning freefall history ---")
+
     last_url = "http://freefall.purrsia.com"
-    next_url = "/ff100/fv00002.htm" # "grayff.htm"
+    # next_url = "grayff.htm"
+    next_url = "/ff200/fv00102.htm"
     while next_url:
         last_url = urljoin(last_url, next_url)
         print(last_url)
         with urlopen(last_url) as response:
             html = str(response.read(), encoding='ISO-8859-1')
-            freefall.append((find(r'<a href="(.+)">.*<img src="\1"', html),
-                             find(r'<title>(.+)</title>', html)))
+            image = find(r'<img src=".*?(\w+.gif)"', html)
+            freefall.append((image,
+                             find(r'<title>(.+)</title>', html),
+                             urljoin(last_url, image)))
             next_url = find(r'<a href="(.+)">Previous</a>', html)
 
+    print("--- downloading files ---")
 
-PATH = 2  # index in parse result
+    freefall_sorted = sorted(freefall, key=lambda x: x[0])
+    for sublist in [freefall_sorted[i:i+10] for i in range(0, len(freefall_sorted), 10)]:
+        print([entry[0] for entry in sublist])
+
 
 main()
